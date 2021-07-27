3efd510f210abc95dba10c0c5b85fe4d424f57a6
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-27 10:51:28 +0300

    Feat(model): update shirt

--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -1,2 +1,9 @@
 class Shirt < ApplicationRecord
+  has_many :favourites
+  has_many :users, through: :favourites
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 100 }
+  validates :price, presence: true
+  validates :image, presence: true
 end
