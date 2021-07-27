3dbe54f0f053bfeaa3bc4e6630e49f194f1e08c4
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-27 10:03:47 +0300

    Feat(sudowear): fix linter errors

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    username { "MyString" }
-    email { "MyString" }
-    image { "MyString" }
-    password_digest { "MyString" }
+    username { 'MyString' }
+    email { 'MyString' }
+    image { 'MyString' }
+    password_digest { 'MyString' }
     admin { false }
   end
 end
