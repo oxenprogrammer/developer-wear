bd187afb977d687a7b0738144e91bc16c2e422f9
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-27 14:35:03 +0300

    Feat(controller): add update shirt

--- app/controllers/api/v1/shirts_controller.rb
+++ app/controllers/api/v1/shirts_controller.rb
@@ -11,13 +11,26 @@ class Api::V1::ShirtsController < ApplicationController
     if shirt.save
       render json: shirt.as_json, status: :created
     else
-      render json: shirt.errors, status: :unprocessable_entity
+      render json: shirt.errors.as_json, status: :unprocessable_entity
     end
   end
 
   def destroy
     shirt = Shirt.find(params[:id]).destroy!
     head :no_content if shirt.destroy
+  end
+
+  def update
+    if shirt
+      shirt.update(shirt_params)
+      if shirt.save
+        render json: shirt.as_json, status: :ok
+      else
+        render json: shirt.errors.as_json, status: :unprocessable_entity
+      end
+    else
+      render json: {status: :not_found, message: 'shirt not found'}, status: :not_found
+    end
   end
 
   private
@@ -31,5 +44,9 @@ class Api::V1::ShirtsController < ApplicationController
 
   def shirt_params
     params.permit(:name, :description, :price, :image)
+  end
+
+  def shirt
+    @shirt ||= Shirt.find_by(id: params.require(:id))
   end
 end
