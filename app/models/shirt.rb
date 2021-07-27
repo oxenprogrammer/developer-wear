c25edf9188dd78117cc14eae073149f16af65485
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-27 11:40:43 +0300

    Feat(model): add image uploader

--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -2,6 +2,8 @@ class Shirt < ApplicationRecord
   has_many :favourites
   has_many :users, through: :favourites
 
+  mount_uploader :image, ImageUploader
+
   validates :name, presence: true, length: { minimum: 3, maximum: 20 }
   validates :description, presence: true, length: { minimum: 10, maximum: 100 }
   validates :price, presence: true
