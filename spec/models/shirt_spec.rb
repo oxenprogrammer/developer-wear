cabe38193a6e14497cc7bff55ebd09a1716fffd4
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-28 18:51:14 +0300

    Test(model): add shirt specs

--- spec/models/shirt_spec.rb
+++ spec/models/shirt_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Shirt, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:shirt) do
+    FactoryBot.create(:shirt, name: 'no coffee', description: 'the best shirt to have is here', price: 345)
+  end
+
+  it 'should have valid attributes' do
+    expect(shirt).to be_valid
+  end
+
+  it 'should not miss any attributes' do
+    @shirt = Shirt.create(name: nil)
+    expect(@shirt).to_not be_valid
+  end
+
+  it { should have_many(:favourites).without_validating_presence }
+
+  it { should have_many(:users).without_validating_presence }
 end
