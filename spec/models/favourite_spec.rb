5377f6c2fd83f77545c1a2507460a4a400e53ca2
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-28 19:25:06 +0300

    Test(model): add favourite specs

--- spec/models/favourite_spec.rb
+++ spec/models/favourite_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Favourite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do
+    FactoryBot.create(:user, username: 'emmisteel', email: 'emmisteel@gmail.com', password: '123456',
+                             password_confirmation: '123456')
+  end
+
+  let(:shirt) do
+    FactoryBot.create(:shirt, name: 'no coffee', description: 'the best shirt to have is here', price: 345)
+  end
+
+  let(:favourite) { FactoryBot.create(:favourite, user_id: user.id, shirt_id: shirt.id)}
+
+  it 'should have valid attributes' do
+    expect(favourite).to be_valid
+  end
+
+  it 'should not miss any attributes' do
+    @favourite = Favourite.create(user_id: nil)
+    expect(@favourite).to_not be_valid
+  end
+
+  it { should belong_to(:user).without_validating_presence }
+
+  it { should belong_to(:shirt).without_validating_presence }
 end
