17ee662d66856ea34b795987a84f7a3ba4ed8eca
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-28 19:10:20 +0300

    Test(model): add user specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user do
     username { 'MyString' }
     email { 'MyString' }
-    image { 'MyString' }
     password_digest { 'MyString' }
     admin { false }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do
+    FactoryBot.create(:user, username: 'emmisteel', email: 'emmisteel@gmail.com', password: '123456',
+                             password_confirmation: '123456')
+  end
+
+  it 'should have valid attributes' do
+    expect(user).to be_valid
+  end
+
+  it 'should not miss any attributes' do
+    @user = User.create(password: nil)
+    expect(@user).to_not be_valid
+  end
+
+  it { should have_many(:favourites).without_validating_presence }
+
+  it { should have_many(:shirts).without_validating_presence }
 end
