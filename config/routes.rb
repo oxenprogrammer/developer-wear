347f75c88e13bd3e8b1feb40ff4ef3fc59fe5f7f
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-28 16:07:12 +0300

    Feat(route): update favourite routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      # shirts Routes
       resources :shirts, only: [:index, :create, :destroy, :update]
+
+      # user Routes
       resources :users, only: [:index, :create, :destroy, :update]
-      # resources :favourites, only: [:index, :create, :destroy, :update]
+
+      # user login Route
       post 'login', to: 'authentication#create'
 
-      # Favourte Route
+      # Favourte Routes
       get 'favourites', to: 'favourites#index'
+      get 'my_favourites', to: 'favourites#show'
       post 'shirts/:id/favourites', to: 'favourites#create'
     end
   end
