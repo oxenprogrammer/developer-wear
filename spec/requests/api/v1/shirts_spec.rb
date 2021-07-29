6b47e92fb9d34240ade5a9c90538fb93171b7142
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-29 11:14:20 +0300

    Test(request): add shirts

--- spec/requests/api/v1/shirts_spec.rb
+++ spec/requests/api/v1/shirts_spec.rb
@@ -2,23 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Shirts', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/api/v1/shirts/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/api/v1/shirts/create'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /destroy' do
-    it 'returns http success' do
-      get '/api/v1/shirts/destroy'
-      expect(response).to have_http_status(:success)
+    it 'should returns http success' do
+      get '/api/v1/shirts',
+          headers: { 'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyfQ.R3jkTSo9lfq6aj-kg7fELRIdE751enHyuBhBz1RgUDs' }
+      expect(response).to have_http_status(:ok)
     end
   end
 end
