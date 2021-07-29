508d4a254018c25f7bf404dab8864b511ac1df87
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-29 11:21:33 +0300

    Test(request): delete auto generated code

--- spec/requests/api/v1/authentication_spec.rb
+++ spec/requests/api/v1/authentication_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Authentications', type: :request do
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/api/v1/authentication/create'
-      expect(response).to have_http_status(:success)
-    end
-  end
+  pending 'reason'
 end
